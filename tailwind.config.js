d7d504027d32e5a4e800a940cd63b1721c465853
Kalana Kaushalya <115646312+Kalana-Kaushalya@users.noreply.github.com>
2023-07-25 21:04:06 +0530

    add code to the repo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
